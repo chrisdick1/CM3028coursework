07a287c908fd0b4c616917f5d07eb4029e6bca86
1308830 <dennis1_obrien@hotmail.com>
2016-11-28 17:38:35 +0000

    cammaaan

---
+++ databaselink.php
@@ -0,0 +1,8 @@
+<?php
+
+define('DB_SERVER','eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME','b89b9a6a20603d');
+define('DB_PASSWORD','6bdb2a67');
+define('DB_DATABASE','butterdatabase');
+
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
