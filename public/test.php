12f16f641a99ac30dff8f69bbe4c0554b00605a9
1409173 <chris49311@hotmail.co.uk>
2016-12-10 18:19:52 +0000

    Testing club page

--- public/test.php
+++ public/test.php
@@ -4,7 +4,40 @@
 
 <?php $layout_context = "public"; ?>
 <?php include("../includes/layouts/header.php"); ?>
-<?php find_selected_page(true); ?>
+<?php find_selected_page(true);
+
+$sql = "SELECT clubid, clubname, clubdescription, clubgenre, clubcontact FROM Clubs";
+$result = $connection->query($sql);?>
+
+    <div class="col-xs-12" id="images">
+
+<?php
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        $clubid = $row["clubid"];
+        $clubname = $row["clubname"];
+        $clubdescription = $row["clubdescription"];
+        $clubgenre = $row["clubgenre"];
+        $clubcontact = $row["clubcontact"];
+
+        ?> <div class="jumbotron">
+
+            <p>Club Name: <?php echo $clubname ?></p>
+            <p>Club Description: <?php echo $clubdescription ?></p>
+            <p>Club Genre: <?php echo $clubgenre ?></p>
+            <p>Club Contact Information: <?php echo $clubcontact ?></p>
+            <?php echo "<a href = '/master/club.php?id=$clubid'>Visit Club Page</a>" ?>
+
+
+        </div>
+        <?php
+    }
+} else {
+    echo "0 results";
+}?>
+
+?>
 
     <div id="main">
         <div id="navigation">
@@ -18,7 +51,28 @@
 
             <?php } else { ?>
 
-                <p>Welcome!</p>
+                <p> <?php
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+            $clubid = $row["clubid"];
+            $clubname = $row["clubname"];
+            $clubdescription = $row["clubdescription"];
+            $clubgenre = $row["clubgenre"];
+            $clubcontact = $row["clubcontact"];
+
+            ?> <div class="jumbotron">
+
+            <p>Club Name: <?php echo $clubname ?></p>
+            <p>Club Description: <?php echo $clubdescription ?></p>
+            <p>Club Genre: <?php echo $clubgenre ?></p>
+            <p>Club Contact Information: <?php echo $clubcontact ?></p>
+            <?php echo "<a href = '/master/club.php?id=$clubid'>Visit Club Page</a>" ?>
+
+
+            </div>
+            <?php
+        } ?></p>
 
 
 
