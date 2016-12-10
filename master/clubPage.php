4b9ff21f96696406768c08ff381d8a0daccbc7ce
1409173 <chris49311@hotmail.co.uk>
2016-12-10 18:05:12 +0000

    ert

--- master/clubPage.php
+++ master/clubPage.php
@@ -22,6 +22,8 @@ if ($result->num_rows > 0) {
         <p>Club Description: <?php echo $clubdescription ?></p>
         <p>Club Genre: <?php echo $clubgenre ?></p>
         <p>Club Contact Information: <?php echo $clubcontact ?></p>
+            <?php echo "<a href = '/photoProfile.php?id=$clubid'>Visit Club Page</a>" ?>
+
 
         </div>
         <?php
@@ -29,3 +31,6 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }?>
+
+
+        <?php echo "<a href = '/photoProfile.php?id=$clubid'>Visit Club Page</a>" ?>
