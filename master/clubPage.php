8a1302e56773ebeba3e8e83328581bd1aae9b449
1409173 <chris49311@hotmail.co.uk>
2016-12-10 17:56:58 +0000

    ert

---
+++ master/clubPage.php
@@ -0,0 +1,31 @@
+<?php
+require_once("../includes/db_connection.php");
+
+$sql = "SELECT clubid, clubname, clubdescription, clubgenre, clubcontact FROM Clubs";
+$result = $connection->query($sql);?>
+
+    <div class="col-xs-12" id="images">
+
+<?php
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+       $clubid = row["clubid"];
+       $clubname = row["clubname"];
+       $clubdescription = row["clubdescription"];
+       $clubgenre = row["clubgenre"];
+       $clubcontact = row["clubcontact"];
+
+       ?> <div class="jumbotron">
+
+        <p>Club Name: <?php echo $clubname ?></p>
+        <p>Club Description: <?php echo $clubdescription ?></p>
+        <p>Club Genre: <?php echo $clubgenre ?></p>
+        <p>Club Contact Information: <?php echo $clubcontact ?></p>
+
+        </div>
+        <?php
+    }
+} else {
+    echo "0 results";
+}?>
---
+++ master/createClub.php
@@ -0,0 +1,9 @@
+<?php
+require_once("../includes/db_connection.php"); 
+
+$_POST[""];
+
+$stmt = "INSERT INTO Clubs (UserID, photoName, description, PhotoLink, price) VALUE ('$id', '$photoName', '$description', '$link', '$price')";
+
+
+?>
