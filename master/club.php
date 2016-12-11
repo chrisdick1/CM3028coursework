b7e40bad56128a0905dbb3c229bedd0a37c55901
1409173 <chris49311@hotmail.co.uk>
2016-12-11 14:58:04 +0000

    Testing club page

--- master/club.php
+++ master/club.php
@@ -6,29 +6,126 @@ $id = $_GET['id'];
 $sql = "SELECT clubid, clubname, clubdescription, clubgenre, clubcontact FROM Clubs WHERE clubid = $id";
 $result = $connection->query($sql);?>
 
-<div class="col-xs-12" id="images">
 
+
+
+    <html lang="en">
+    <head>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="../../favicon.ico">
+
+        <title>Jumbotron Template for Bootstrap</title>
+
+        <!-- Bootstrap core CSS -->
+        <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+        <!-- Custom styles for this template -->
+        <link href="jumbotron.css" rel="stylesheet">
+
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+        <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+
+    <body>
+    <br>
     <?php
-    if ($result->num_rows > 0) {
-        // output data of each row
-        while($row = $result->fetch_assoc()) {
-            $clubid = $row["clubid"];
-            $clubname = $row["clubname"];
-            $clubdescription = $row["clubdescription"];
-            $clubgenre = $row["clubgenre"];
-            $clubcontact = $row["clubcontact"];
-
-            ?> <div class="jumbotron">
-
-                <p>Club Name: <?php echo $clubname ?></p>
-                <p>Club Description: <?php echo $clubdescription ?></p>
-                <p>Club Genre: <?php echo $clubgenre ?></p>
-                <p>Club Contact Information: <?php echo $clubcontact ?></p>
+    require_once("../includes/db_connection.php");
 
 
-            </div>
+    $sql = "SELECT clubid, clubname, clubdescription, clubgenre, clubcontact FROM Clubs";
+    $result = $connection->query($sql);?>
+
+    <div class="col-xs-12" id="images">
+        <?php
+        $sql = "SELECT clubid, clubname, clubdescription, clubgenre, clubcontact FROM Clubs WHERE clubid = $id";
+        $result = $connection->query($sql);?>
+
+        <div class="col-xs-12" id="images">
+
             <?php
-        }
-    } else {
-        echo "0 results";
-    }?>
+            if ($result->num_rows > 0) {
+                // output data of each row
+                while($row = $result->fetch_assoc()) {
+                    $clubid = $row["clubid"];
+                    $clubname = $row["clubname"];
+                    $clubdescription = $row["clubdescription"];
+                    $clubgenre = $row["clubgenre"];
+                    $clubcontact = $row["clubcontact"];
+
+                    ?> <div class="jumbotron">
+
+                        <p>Club Name: <?php echo $clubname ?></p>
+                        <p>Club Description: <?php echo $clubdescription ?></p>
+                        <p>Club Genre: <?php echo $clubgenre ?></p>
+                        <p>Club Contact Information: <?php echo $clubcontact ?></p>
+
+
+                    </div>
+                    <?php
+                }
+            } else {
+                echo "0 results";
+            } ?>
+
+
+
+
+
+
+
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Project name</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                    <form class="navbar-form navbar-right">
+                        <div class="form-group">
+                            <input type="text" placeholder="Email" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" placeholder="Password" class="form-control">
+                        </div>
+                        <button type="submit" class="btn btn-success">Sign in</button>
+                    </form>
+                </div><!--/.navbar-collapse -->
+            </div>
+        </nav>
+
+
+
+
+
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+        <script src="../../dist/js/bootstrap.min.js"></script>
+        <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+        <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+    </body>
+    </html>
