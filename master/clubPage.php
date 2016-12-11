7a19c434555d4f499c75c895788b20db1c79dc2b
1409173 <chris49311@hotmail.co.uk>
2016-12-11 14:49:49 +0000

    Testing club page

--- master/clubPage.php
+++ master/clubPage.php
@@ -37,36 +37,38 @@ if ($result->num_rows > 0) {
         <!DOCTYPE html>
         <html lang="en">
         <head>
-            <!-- Latest compiled and minified CSS -->
-            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-            <meta name="description" content="">
-            <meta name="author" content="">
-            <link rel="icon" href="../../favicon.ico">
 
-            <title>Jumbotron Template for Bootstrap</title>
 
-            <!-- Bootstrap core CSS -->
-            <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+                <meta charset="utf-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1">
+                <meta name="description" content="">
+                <meta name="author" content="">
 
-            <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-            <link href="../../assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+                <title>Modern Business - Start Bootstrap Template</title>
+
+                <!-- Bootstrap Core CSS -->
+                <link href="css/bootstrap.min.css" rel="stylesheet">
+
+                <!-- Custom CSS -->
+                <link href="css/modern-business.css" rel="stylesheet">
+
+                <!-- Custom Fonts -->
+                <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+                <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+                <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+                <!--[if lt IE 9]>
+                <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+                <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+                <![endif]-->
+
+
 
             <!-- Custom styles for this template -->
             <link href="jumbotron.css" rel="stylesheet">
 
-            <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-            <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-            <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
 
-            <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-            <!--[if lt IE 9]>
-            <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-            <![endif]-->
         </head>
 
         <body>
