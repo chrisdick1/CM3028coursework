d8570e41b119d9178c47e242ba2162f564f5bd4e
1409173 <chris49311@hotmail.co.uk>
2016-12-11 17:21:03 +0000

    admin

--- master/verify.php
+++ master/verify.php
@@ -5,7 +5,7 @@ require_once("../includes/db_connection.php");
 $id =  $_POST["id"];
 
 
-$stmt = new mysqli_stmt ($connection, "UPDATE tbl_users SET permissions = 3 WHERE id = ?");
+$stmt = new mysqli_stmt ($connection, "UPDATE tbl_users SET permissions = 3 WHERE user_id = ?");
 if ($stmt) {
     $stmt->bind_param("i", $id);
     $stmt->execute();
