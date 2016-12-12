062fc47c4f25c08c04d5bd35d7e9a9d0d4397bd1
DenadaPatron <artukras@gmail.com>
2016-12-12 09:01:42 +0000

    Creating a separate page for log-in, but not modal

---
+++ master/logIn.php
@@ -0,0 +1,106 @@
+<?php
+include ("header.php");
+?>
+
+<?php
+session_start();
+require_once("../includes/db_connection.php");
+
+if (isset($_SESSION['userSession'])!="") {
+    header("Location: home.php");
+    exit;
+}
+
+if (isset($_POST['btn-login'])) {
+
+    $email = strip_tags($_POST['email']);
+    $password = strip_tags($_POST['password']);
+
+    $email = $connection->real_escape_string($email);
+    $password = $connection->real_escape_string($password);
+
+    $query = $connection->query("SELECT user_id, username, email, password, permissions FROM tbl_users WHERE email='$email'");
+    $row=$query->fetch_array();
+
+    $count = $query->num_rows; // if email/password are correct returns must be 1 row
+
+    if (password_verify($password, $row['password']) && $count==1) {
+        $_SESSION['userSession'] = $row['user_id'];
+        $_SESSION['username'] = $row['username'];
+        $_SESSION['permissions'] = $row['permissions'];
+
+        header("Location: home.php");
+    } else {
+        $msg = "<div class='alert alert-danger'>
+     <span class='glyphicon glyphicon-info-sign'></span> &nbsp; Invalid Username or Password !
+    </div>";
+    }
+    $connection->close();
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <!-- Latest compiled and minified CSS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa "
+            crossorigin="anonymous"></script>
+</head>
+
+<div class="signin-form">
+
+    <div class="container">
+
+
+        <form class="form-signin" method="post" id="login-form">
+
+            <h2 class="form-signin-heading">Sign In.</h2><hr />
+
+            <?php
+            if(isset($msg)){
+                echo $msg;
+            }
+            ?>
+
+            <div class="form-group">
+                <input type="email" class="form-control" placeholder="Email address" name="email" required />
+                <span id="check-e"></span>
+            </div>
+
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password" name="password" required />
+            </div>
+
+            <hr />
+
+            <div class="form-group">
+                <button type="submit" class="btn btn-default" name="btn-login" id="btn-login">
+                    <span class="glyphicon glyphicon-log-in"></span> &nbsp; Sign In
+                </button>
+
+                <a href="register.php" class="btn btn-default" style="float:right;">Sign UP Here</a>
+
+            </div>
+
+
+
+        </form>
+
+    </div>
+
+</div>
+
+
+</body>
+</html>
