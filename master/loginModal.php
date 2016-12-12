cb61a871758c98199ad4d2b3b1bf19bed63cd53e
DenadaPatron <artukras@gmail.com>
2016-12-12 08:06:12 +0000

    tweaking main page to accept modal login (3)

--- master/loginModal.php
+++ master/loginModal.php
@@ -10,7 +10,7 @@ require_once("../includes/db_connection.php");
 <?php
 require_once("../includes/db_connection.php");
 ?>
-
+ho
 
 
 
@@ -18,8 +18,12 @@ require_once("../includes/db_connection.php");
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
-    <!-- Latest compiled and minified CSS -->
 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="../../dist/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
           integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -100,13 +104,5 @@ require_once("../includes/db_connection.php");
 </div>>--
 
 
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-<script src="../../dist/js/bootstrap.min.js"></script>
-<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-<script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
