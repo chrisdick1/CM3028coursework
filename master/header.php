baa59f331c2959a1f00ceab231440b947f4a7225
DenadaPatron <artukras@gmail.com>
2016-12-12 06:51:41 +0000

    Trying the new navbar out.. (5)

--- master/header.php
+++ master/header.php
@@ -52,10 +52,11 @@ session_start();
                 </div>
                 <div class="collapse navbar-collapse" id="mainNavBar">
                     <?php
-
+                    echo"<ul class='nav navbar-nav'>";
                     echo"<li><a href='/master/clubPage.php'>Home</a></li>";
                     echo "<li><a href='/master/logout.php'>Clubs</a></li>";
                     echo "<li><a href='/master/Health.php'>Health</a></li>";
+                    echo "</ul>;"
 
 
                     if (isset($_SESSION['userSession' ])) {
