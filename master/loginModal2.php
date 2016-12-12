da029502215a442b584812a8b062a2408f5acdd8
1408229 <artukras@gmail.com>
2016-12-12 15:44:16 +0000

    unpatience

--- master/loginModal2.php
+++ master/loginModal2.php
@@ -33,27 +33,27 @@ if (isset($_POST['btn-login'])) {
 }
 ?>
 <html>
-<!--<head>-->
-<!--    <title>Log In</title>-->
-<!--    <meta charset="utf-8">-->
-<!--    <!-- Latest compiled and minified CSS -->-->
-<!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>-->
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"-->
-<!--          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
-<!---->
-<!--    <!-- Optional theme -->-->
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"-->
-<!--          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" >-->
-<!---->
-<!--    <!-- Latest compiled and minified JavaScript -->-->
-<!--    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"-->
-<!--            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa "-->
-<!--            crossorigin="anonymous"></script>-->
-<!--<body>-->
-<!---->
-<!--<div class="container">-->
-<!---->
-<!--    <h3>Log In Demo</h3>-->
+<head>
+    <title>Log In</title>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+          integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous" >
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+            integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa "
+            crossorigin="anonymous"></script>
+<body>
+
+<div class="container">
+
+    <h3>Log In Demo</h3>
 
 
     <!-- data-toggle lets you display modal without any JavaScript -->
