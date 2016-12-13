31d26f54a687510b086c7ebbeba2124eda85b5da
DenadaPatron <artukras@gmail.com>
2016-12-13 15:34:35 +0000

    test comment out text  for styling

--- master/clubPage.php
+++ master/clubPage.php
@@ -63,7 +63,7 @@ if ($result->num_rows > 0) {
 
        ?>
 
-         <div class="col-lm-4 col-md-4 col-sm-6" >
+         <div class="col-lm-3 col-md-6 col-sm-6" >
 
                 <div class="panel panel-default text-center">
                     <div class="panel-heading">
