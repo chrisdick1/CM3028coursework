e4ad00842bd89022e903e036e142153e48d2063a
DenadaPatron <artukras@gmail.com>
2016-12-13 04:10:05 +0000

    home header and image push

--- master/index.php
+++ master/index.php
@@ -2,6 +2,20 @@
 include ("header.php");
 ?>
 
+<header class="intro-header" style="background-image: url('img/home-bg.jpg')">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="site-heading">
+                    <h1>Clean Blog</h1>
+                    <hr class="small">
+                    <span class="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
 <?php
 /*if you are coming from logout page -> shout this out\*/
 if(!empty($_GET['status'])){
