8766360d2d2ae1020464b0299ffb73ab0d2a6abd
DenadaPatron <artukras@gmail.com>
2016-12-13 14:33:19 +0000

    test comment out text  for styling

--- master/deleteHealth.php
+++ master/deleteHealth.php
@@ -4,7 +4,7 @@ require_once("../includes/db_connection.php");
 $eventID = $_POST["eventID"];
 
 //sql to delete a group
-$sql = "DELETE FROM health WHERE eventName = $eventID";
+$sql = "DELETE FROM health WHERE eventID = $eventID";
 
 if (mysqli_query($connection, $sql)) {
     echo "New record created successfully";
