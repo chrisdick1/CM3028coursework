92a0849d3e6df28b09315fb3d85999e632901b26
DenadaPatron <artukras@gmail.com>
2016-12-13 05:25:16 +0000

    home header and image push

--- master/index.php
+++ master/index.php
@@ -26,13 +26,24 @@ if(!empty($_GET['status'])){ ?>
                 <div class="site-heading">
                     <h1>Go Portlethen</h1>
                     <hr class="small">
+                    <span class="subheading">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus augue nec justo dignissim maximus. Etiam eleifend tortor a magna sagittis, nec varius mauris accumsan. Vestibulum
+                        vitae felis vitae odio ultrices finibus laoreet in felis. Aliquam ex risus, interdum eget velit ut, luctus interdum justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+
+                        himenaeos. Suspendisse dapibus blandit vestibulum. Mauris tempus orci sit amet vulputate lacinia. Duis eget nunc non augue gravida interdum sed et magna. Class aptent taciti sociosqu ad litora torquent per
+                        conubia nostra, per inceptos himenaeos. Sed eget nulla viverra, venenatis orci et, egestas nibh. Ut luctus sapien sagittis dui sodales, ac dictum quam laoreet. Vestibulum ut nunc blandit arcu posuere tinci
+                        dunt.
+
+
+</span>
+                    <hr class="small">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam dapibus augue nec justo dignissim maximus. Etiam eleifend tortor a magna sagittis, nec varius mauris accumsan.
+                    Vestibulum vitae felis vitae odio ultrices finibus laoreet in felis. Aliquam ex risus, interdum eget velit ut, luctus interdum justo. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+                    per inceptos himenaeos. Suspendisse dapibus blandit vestibulum. Mauris tempus orci sit amet vulputate lacinia. Duis eget nunc non augue gravida interdum sed et magna. Class aptent taciti sociosqu ad litora
+                    torquent per conubia nostra, per inceptos himenaeos. Sed eget nulla viverra, venenatis orci et, egestas nibh. Ut luctus sapien sagittis dui sodales, ac dictum quam laoreet. Vestibulum ut nunc blandit arcu
+                    posuere tincidunt.
+
+
                     <span class="subheading">1914 translation by H. Rackham
-
-"But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but because occasionally circumstances occur in which toil and pain can procure him some great pleasure. To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure?"
-
-Section 1.10.33 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC
-
-"At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat."</span>
+</span>
                 </div>
             </div>
         </div>
