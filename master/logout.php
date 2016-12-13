7af29e00cd644601032de0f8b28763390f951285
DenadaPatron <artukras@gmail.com>
2016-12-13 02:37:27 +0000

    adding logout session message!

--- master/logout.php
+++ master/logout.php
@@ -1,20 +1,36 @@
 <?php
+
+
+// Inialize session
 session_start();
 
+// Delete certain session
+unset($_SESSION['userSession']);
+unset($_SESSION['username']);
+// Delete all session variables
+session_destroy();
+
+// Jump to login page
+header('/master/index.php?status=loggedout');
+
+?>
+//session_start();
+//
 //unset($_SESSION['userSession']);
 //unset($_SESSION['username']);
-
-if (!isset($_SESSION['userSession'])) {
-    header("Location: clubPage.php");
-} else if (isset($_SESSION['userSession'])!="") {
-    header("Location: clubPage.php");
-}
-
-if (isset($_GET['logout'])){
-    session_destroy();
-    unset($_SESSION['userSession']);
-    unset($_SESSION['username']);
-
-    header("location: /master/index.php?status=loggedout");
+//
+//if (!isset($_SESSION['userSession'])) {
 //    header("Location: clubPage.php");
-}
+//} else if (isset($_SESSION['userSession'])!="") {
+//    header("Location: clubPage.php");
+//}
+//
+//if (isset($_GET['logout'])){
+//    session_destroy();
+//    unset($_SESSION['userSession']);
+//    unset($_SESSION['username']);
+//
+//    header("location: /master/index.php?status=loggedout");
+//    header("Location: clubPage.php");
+//}
+//?>
