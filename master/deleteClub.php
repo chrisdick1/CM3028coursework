034e378572e5347cb7806e99a9db1a567f559da3
DenadaPatron <artukras@gmail.com>
2016-12-13 23:21:40 +0000

    final push--> everythin is simple but works

--- master/deleteClub.php
+++ master/deleteClub.php
@@ -4,14 +4,14 @@ require_once("../includes/db_connection.php");
 $clubid = $_POST["clubid"];
 
 //sql to delete a group
-$sql = "DELETE FROM clubs WHERE clubid = '.$clubid.'";
+$sql = "DELETE FROM clubs WHERE clubid = '$clubid.'";
 
 if (mysqli_query($connection, $sql)) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($connection);
 }
-echo '$clubid';
 
-header("Location: clubPage.php")  ;
+
+//header("Location: clubPage.php")  ;
 ?>"
