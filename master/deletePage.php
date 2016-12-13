54fa3c6cb026b5ca24046e1aaea8fa82eb2fb2ca
DenadaPatron <artukras@gmail.com>
2016-12-13 11:57:45 +0000

    creating delete button

--- master/deletePage.php
+++ master/deletePage.php
@@ -2,7 +2,7 @@
 require_once("../includes/db_connection.php");
 
 //sql to delete a group
-$sql = "DELETE FROM clubs WHERE clubid = $clubID";
+$sql = "DELETE FROM clubs WHERE clubid = $clubid";
 
 if (mysqli_query($connection, $stmt)) {
     echo "New record created successfully";
