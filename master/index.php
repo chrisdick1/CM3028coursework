aeb1f49539ed8ee0533b557b8330714915afb0ed
DenadaPatron <artukras@gmail.com>
2016-12-13 04:41:29 +0000

    home header and image push

--- master/index.php
+++ master/index.php
@@ -1,14 +1,25 @@
 <?php include ("header.php"); ?>
-<style>   .col-lg-8{
-                    height: 300px;
-         }
 
-            .col-md-10 {
-                height: 300px;
-            }
-        h1{
-            color:white
-        }
+<?php
+if(!empty($_GET['status'])){
+    echo '<div class=container-fluid';
+    echo '<div class="col-md-12">$';
+    echo '<div class="alert alert-success">
+<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</Logged out successfully </div>';
+    echo '</div>';
+    echo '</div>';
+}
+?>
+<style>   .col-lg-8{
+        height: 300px;
+    }
+
+    .col-md-10 {
+        height: 300px;
+    }
+    h1{
+        color:white
+    }
 
 </style>
 <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
