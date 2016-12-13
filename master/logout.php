ce066f0b7beb499e12495152707d4485aab359de
DenadaPatron <artukras@gmail.com>
2016-12-13 02:41:10 +0000

    adding logout session message!3

--- master/logout.php
+++ master/logout.php
@@ -5,13 +5,19 @@
 session_start();
 
 // Delete certain session
+session_destroy();
 unset($_SESSION['userSession']);
 unset($_SESSION['username']);
 // Delete all session variables
-session_destroy();
+if (!isset($_SESSION['username'])==null)
+{
+    header('/master/index.php?status=loggedout');
+}
+
+
 
 // Jump to login page
-header('/master/index.php?status=loggedout');
+
 
 ?>
 //session_start();
