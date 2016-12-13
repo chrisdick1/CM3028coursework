b64b68d16a287d0322eee3e068fc43b4049d006c
DenadaPatron <artukras@gmail.com>
2016-12-13 04:38:33 +0000

    home header and image push

--- master/index.php
+++ master/index.php
@@ -5,6 +5,9 @@
 
             .col-md-10{
                  height: 300px;
+            .h1{
+                color:white
+            }
          }
 </style>
 <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
@@ -12,7 +15,7 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="site-heading">
-                    <h1>Clean Blog</h1>
+                    <h1>Go Portlethen</h1>
                     <hr class="small">
                     <span class="subheading">A Clean Blog Theme by Start Bootstrap</span>
                 </div>
