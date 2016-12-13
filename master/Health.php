f03c579b08dcb73aeeb3a3ebb3dcb107df7cca38
DenadaPatron <artukras@gmail.com>
2016-12-13 18:14:11 +0000

    finalizing health

--- master/Health.php
+++ master/Health.php
@@ -84,7 +84,7 @@ $result = $connection->query($sql);?>
                 <h3>Event: <?php echo $eventName ?></h3>
                 <p><?php echo $description ?></p>
                 <p>Date <?php echo $date ?></p>
-                <form action="deleteClub.php" method="post">
+                <form action="deleteHealth.php" method="post">
                     <input type="hidden" name="eventID" value='<?php echo $eventID; ?>'>
                     <input type="submit" value="Delete">
                 </form>
