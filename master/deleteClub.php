1c7eabd6f0f1bd1e6f57051e89c64f964f80371a
DenadaPatron <artukras@gmail.com>
2016-12-13 23:32:27 +0000

    final push--> everythin is simple but works

--- master/deleteClub.php
+++ master/deleteClub.php
@@ -8,10 +8,11 @@ $sql = "DELETE FROM clubs WHERE clubid = '.$clubid.'";
 
 if (mysqli_query($connection, $sql)) {
     echo "New record created successfully";
+    echo $clubid;
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($connection);
 }
-echo $clubid;
+
 
 
 //header("Location: clubPage.php")  ;
