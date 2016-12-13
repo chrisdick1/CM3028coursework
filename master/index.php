14d4e3ac3479a6ee859fcf200bec360267cb8bcb
DenadaPatron <artukras@gmail.com>
2016-12-13 18:26:24 +0000

    finalizing health

--- master/index.php
+++ master/index.php
@@ -22,7 +22,7 @@ if(!empty($_GET['status'])){ ?>
 <?php }?>
 <header class="intro-header" style="background-image: url('img/home-bg.jpg')">
     <div class="container">
-<!--        <div class="row">-->
+<!--         <div class="row">-->
 <!--            <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">-->
 <!---->
 <!--                    <h1>Go Portlethen</h1>-->
