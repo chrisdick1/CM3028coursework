d64d97fad2361f826383b9239f52ffb8c56b02ce
DenadaPatron <artukras@gmail.com>
2016-12-13 02:43:10 +0000

    adding logout session message!3

--- master/logout.php
+++ master/logout.php
@@ -1,22 +1,21 @@
 <?php
-
-
-// Inialize session
 session_start();
 
-// Delete certain session
-session_destroy();
 unset($_SESSION['userSession']);
 unset($_SESSION['username']);
-// Delete all session variables
-if (!isset($_SESSION['username'])==null)
-{
-    header('/master/index.php?status=loggedout');
+
+if (!isset($_SESSION['userSession'])) {
+    header("Location: clubPage.php");
+} else if (isset($_SESSION['userSession'])!="") {
+    header("Location: clubPage.php");
 }
 
+if (isset($_GET['logout'])){
+    session_destroy();
+    unset($_SESSION['userSession']);
+    unset($_SESSION['username']);
 
-
-// Jump to login page
-
-
-?>
+    header("location: /master/index.php?status=loggedout");
+    header("Location: clubPage.php");
+}
+//?>
